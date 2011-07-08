9fbbd0a04d192184c767afa4912dd3fb6847d683
Jeff Squires <jeff.squires@gmail.com>
2011-07-07 23:13:58 -0400

    add a new promotion action: give store credit

--- lib/spree_store_credits.rb
+++ lib/spree_store_credits.rb
@@ -6,7 +6,9 @@ module SpreeStoreCredits
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env == "production" ? require(c) : load(c)
       end
+      Promotion::Actions::GiveStoreCredit.register
     end
+
     config.to_prepare &method(:activate).to_proc
     config.autoload_paths += %W(#{config.root}/lib)
   end
