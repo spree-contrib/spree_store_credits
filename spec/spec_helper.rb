bf95973e885d0a84c23e9e72ce9d64823ef07eed
Jeff Squires <jeff.squires@gmail.com>
2011-07-08 13:53:27 -0400

    get rid of debugger statement in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 :# This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-debugger
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../test_app/config/environment", __FILE__)
 require 'rspec/rails'
