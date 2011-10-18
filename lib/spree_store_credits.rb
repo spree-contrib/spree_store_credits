c5159ef85576b2da960f214c03ddff13397ddb58
Jeff Squires <jeff.squires@gmail.com>
2011-10-18 18:40:18 -0400

    change to new rails31 registraion mechanism for calculators/promotion actions/etc

--- lib/spree_store_credits.rb
+++ lib/spree_store_credits.rb
@@ -6,18 +6,14 @@ module SpreeStoreCredits
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env == "production" ? require(c) : load(c)
       end
-
-      # this line is so important (borrowed from spree_promo).  I don't know why, but there's a chicken/egg
-      # issue when running rake test_app.
-      # the migrations fail b/c the app gets its activate method called, which tries regist a promotion, but
-      # the activator table doesn't yet exist.
-      if Activator.table_exists?
-        Promotion::Actions::GiveStoreCredit.register
-      end
     end
 
     config.to_prepare &method(:activate).to_proc
     config.autoload_paths += %W(#{config.root}/lib)
+
+    initializer "spree.promo.register.promotions.actions" do |app|
+      app.config.spree.promotions.actions.concat([Promotion::Actions::GiveStoreCredit])
+    end
   end
 end
 
