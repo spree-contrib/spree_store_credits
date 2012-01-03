b491e5aeef50d0b3065a1f4025b6a24e90705f42
David North <davidnorth@railsdog.com>
2012-01-03 11:48:10 +0000

    Paginate admin store credits list

--- app/controllers/admin/store_credits_controller.rb
+++ app/controllers/admin/store_credits_controller.rb
@@ -1,7 +1,7 @@
 class Admin::StoreCreditsController < Admin::ResourceController
   before_filter :check_amounts, :only => [:edit, :update]
   prepend_before_filter :set_remaining_amount, :only => [:create, :update]
-  
+
   private
   def check_amounts
     if (@store_credit.remaining_amount < @store_credit.amount)
@@ -9,8 +9,13 @@ class Admin::StoreCreditsController < Admin::ResourceController
       redirect_to admin_store_credits_path
     end
   end
-  
+
   def set_remaining_amount
     params[:store_credit][:remaining_amount] = params[:store_credit][:amount]
   end
+
+  def collection
+    super.page(params[:page])
+  end
+
 end
