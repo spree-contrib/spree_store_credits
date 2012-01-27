c4b6c01e95699b4f04efd2c4c7b63fe93dac08de
Brian Quinn <briandquinn@gmail.com>
2012-01-27 16:52:54 +0000

    Add more unique name to namespace migration, just in case

--- db/migrate/20120127100416_namespace_store_credits_tables.rb
+++ db/migrate/20120127100416_namespace_store_credits_tables.rb
@@ -1,4 +1,4 @@
-class NamespaceTables < ActiveRecord::Migration
+class NamespaceStoreCreditsTables < ActiveRecord::Migration
   def change
     rename_table :store_credits, :spree_store_credits
   end
