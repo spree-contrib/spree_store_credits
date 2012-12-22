59fe0ac07d22b7200f3d0e253264f77ad7681efe
Musannif Zahir <mzahir@gmail.com>
2012-12-22 20:32:48 +0500

    Change admin config view per admin redesign in 1.3
    Fix admin views_decorator.rb
    
    Remove manage store credits text
    
    Update promo partial and add preference_amount as attr_accessible (per the rails 3.2 security update)

--- app/models/spree/promotion/actions/give_store_credit.rb
+++ app/models/spree/promotion/actions/give_store_credit.rb
@@ -1,12 +1,11 @@
-module Spree
-  class Promotion::Actions::GiveStoreCredit < PromotionAction
-    preference :amount, :decimal, :default => 0.0
+class Spree::Promotion::Actions::GiveStoreCredit < Spree::PromotionAction
+  preference :amount, :decimal, :default => 0.0
+  attr_accessible :preferred_amount
 
-    def perform(options = {})
-      if user = options[:user]
-        user.store_credits.create(:amount => preferred_amount, :remaining_amount => preferred_amount,  :reason => "Promotion: #{promotion.name}")
-      else
-      end
+  def perform(options = {})
+    if user = options[:user]
+      user.store_credits.create(:amount => preferred_amount, :remaining_amount => preferred_amount,  :reason => "Promotion: #{promotion.name}")
+    else
     end
   end
 end
